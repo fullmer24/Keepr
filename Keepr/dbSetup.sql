829a33402519ca54d93a985492fdf5da188a48c7
Kade Fullmer <fullmer24@gmail.com>
2022-09-23 11:23:27 -0600

    tables completed

--- Keepr/dbSetup.sql
+++ Keepr/dbSetup.sql
@@ -6,3 +6,37 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+-- NOTE keeps
+CREATE TABLE IF NOT EXISTS keeps(
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(1000) NOT NULL,
+  img TEXT NOT NULL,
+  views INT DEFAULT 0,
+  kept INT DEFAULT 0,
+  vaultKeepId INT,
+  FOREIGN KEY (creatorId) REFERENCES accounts (id)
+)default charset utf8 COMMENT '';
+
+-- NOTE vaults
+CREATE TABLE IF NOT EXISTS vaults(
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(1000) NOT NULL,
+  isPrivate BOOLEAN DEFAULT false,
+  FOREIGN KEY (creatorId) REFERENCES accounts (id)
+)default charset utf8 COMMENT '';
+
+-- NOTE vaultKeep
+CREATE TABLE IF NOT EXISTS vaultKeep(
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  vaultId INT NOT NULL,
+  keepId INT NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts (id),
+  FOREIGN KEY (vaultId) REFERENCES vaults (id),
+  FOREIGN KEY (keepId) REFERENCES keeps (id)
+)default charset utf8 COMMENT '';
