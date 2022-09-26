cb425ba3b23942cba4b810961baa50111b6da222
Kade Fullmer <fullmer24@gmail.com>
2022-09-26 12:11:10 -0600

    Front end started, Keeps files created

--- Keepr.client/src/router.js
+++ Keepr.client/src/router.js
@@ -12,9 +12,10 @@ const routes = [
     component: loadPage('HomePage')
   },
   {
-    path: '/about',
-    name: 'About',
-    component: loadPage('AboutPage')
+    path: '/keeps/:id',
+    name: 'Keep Details',
+    component: loadPage('KeepDetailsPage'),
+    beforeEnter: authSettled
   },
   {
     path: '/account',
