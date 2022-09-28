dfa0d8955227aba16a2945bf1dbeb6ef4144c97a
Kade Fullmer <fullmer24@gmail.com>
2022-09-28 15:30:13 -0600

    vm name corrected

--- Keepr/Models/Keeps.cs
+++ Keepr/Models/Keeps.cs
@@ -7,13 +7,11 @@ namespace Keepr.Models
         public string Img { get; set; }
         public int Views { get; set; }
         public int Kept { get; set; }
-        public int VaultKeepId { get; set; }
         public string creatorId { get; set; }
         public Account Creator { get; set; }
-        // NOTE possibly replicate line above for vaultKeepId
     }
     public class VaultKeepsVM : Keeps
     {
-        public int MyVaultKeepsId { get; set; }
+        public int VaultKeepsId { get; set; }
     }
 }
